3801fff1c59b5a41a3c44427dc3900db64b865bc
Tim Lovorn <tflovorn@crimson.ua.edu>
2011-05-15 12:39:30 -0500

    dbman now managed by test object

--- test_evedb.py
+++ test_evedb.py
@@ -2,30 +2,31 @@ import unittest
 import evedb
 
 class KnownValueItems(unittest.TestCase):
+    def setUp(self):
+        self.dbman = evedb.DB_Manager("evedb", "postgres", "postgrespass")
+
+    def tearDown(self):
+        self.dbman.close()
+
     def test_name_Tritanium(self):
-        dbman = evedb.DB_Manager("evedb", "postgres", "postgrespass")
-        item = dbman.item_by_name("Tritanium")
+        item = self.dbman.item_by_name("Tritanium")
         self.assertEqual(item, ("Tritanium", 34, 0.01, 18))
 
     def test_id_34(self):
-        dbman = evedb.DB_Manager("evedb", "postgres", "postgrespass")
-        item = dbman.item_by_id(34)
+        item = self.dbman.item_by_id(34)
         self.assertEqual(item, ("Tritanium", 34, 0.01, 18))
 
     def test_material_18(self):
-        dbman = evedb.DB_Manager("evedb", "postgres", "postgrespass")
-        materials = dbman.materials(18)
+        materials = self.dbman.materials(18)
         self.assertEqual(materials, [(18, 34, 256), (18, 35, 512), 
                 (18, 36, 256)])
 
     def test_market_group_5(self):
-        dbman = evedb.DB_Manager("evedb", "postgres", "postgrespass")
-        market_group = dbman.market_group(5)
+        market_group = self.dbman.market_group(5)
         self.assertEqual(market_group, ("Standard Frigates", 5, 1361))
 
     def test_market_group_with_NULL(self):
-        dbman = evedb.DB_Manager("evedb", "postgres", "postgrespass")
-        market_group = dbman.market_group(2)
+        market_group = self.dbman.market_group(2)
         self.assertEqual(market_group[2], None)
 
 if __name__ == "__main__":
